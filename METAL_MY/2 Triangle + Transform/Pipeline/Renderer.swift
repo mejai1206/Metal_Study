4b885173a2175ce2a77128d8d39fa201cf3d3a33
mejai.p <mejai.p@kakaobrain.com>
2023-01-25 18:16:39 +0900

    triangle+transform

--- METAL_MY/2 Triangle + Transform/Pipeline/Renderer.swift
+++ METAL_MY/2 Triangle + Transform/Pipeline/Renderer.swift
@@ -1,5 +1,5 @@
 import MetalKit
-import Common
+
 
 class Renderer: NSObject {
     
@@ -7,6 +7,7 @@ class Renderer: NSObject {
     static var commandQueue: MTLCommandQueue!
     var vertexBuffer: MTLBuffer!
     var pipelineState: MTLRenderPipelineState!
+    var angle: Int = 0
     
     init(metalView: MTKView) {
         guard
@@ -27,13 +28,13 @@ class Renderer: NSObject {
         pipelineDescriptor.vertexFunction = vertexFunction
         pipelineDescriptor.fragmentFunction = fragmentFunction
         pipelineDescriptor.colorAttachments[0].pixelFormat = metalView.colorPixelFormat
-                        
+        
         var vertexData: [Float] = [
-                    //x     y    z       r    g    b
-                    -0.8,  0.4, 0.0,    1.0, 0.0, 0.0,
-                     0.4, -0.8, 0.0,    0.0, 1.0, 0.0,
-                     0.8,  0.8, 0.0,    0.0, 0.0, 1.0,
-                ]
+            //x     y    z       r    g    b
+            -0.8,  0.4, 0.0,    1.0, 0.0, 0.0,
+             0.4, -0.8, 0.0,    0.0, 1.0, 0.0,
+             0.8,  0.8, 0.0,    0.0, 0.0, 1.0
+        ]
         
         //device에 활용될 새 버퍼메모리를 할당하고 vertexData복사.
         //처리해야 할 device가 알아야 하는 버퍼의 형태.
@@ -42,7 +43,7 @@ class Renderer: NSObject {
                                          options: .storageModeShared) //!?
         
         let vertexDescriptor = MTLVertexDescriptor()
-
+        
         vertexDescriptor.attributes[0].format = .float3 //x, y, z
         vertexDescriptor.attributes[0].offset = 0
         vertexDescriptor.attributes[0].bufferIndex = 0
@@ -75,8 +76,8 @@ extension Renderer: MTKViewDelegate {
     }
     
     
-//    The draw(in:) method is called periodically so we have the chance to redraw the contents of the view with Metal.
-//    This is where we will do most of our work.
+    //    The draw(in:) method is called periodically so we have the chance to redraw the contents of the view with Metal.
+    //    This is where we will do most of our work.
     func draw(in view: MTKView) {
         guard
             let renderPassDescriptor = view.currentRenderPassDescriptor,
@@ -87,17 +88,46 @@ extension Renderer: MTKViewDelegate {
         }
         
         renderCommandEncoder.setRenderPipelineState(pipelineState)
-        renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0) //그리고자 하는 셰이더에 필요한 전역 vertex buffer 설정.
+        renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        //그리고자 하는 셰이더에 필요한 전역 vertex buffer 설정.
         // vertex function argument table중 buffer항목 첫번째 인덱스에 설정
         
+        let aspect = Float(view.bounds.width) / Float(view.bounds.height)
         
-        //transform
-        let aspect = Float(view.bounds.height) / Float(view.bounds.width)
-        var scaleMat = matrix_identity_float4x4
-        //scaleMat.columns.0 = [aspect, 0.0, 0.0, 0.0];
-        renderCommandEncoder.setVertexBytes(&scaleMat, length:MemoryLayout<float4x4>.stride, index: 1)
+        let scale = float4x4(
+            [50,    0,  0,   0],
+            [0,    50,  0,   0],
+            [0,     0,  1,   0],
+            [0,     0,  0,   1]
+        )
         
-        let un = Uniforms()
+        angle = (angle + 1) % 360
+        let rad = Float(angle).degreesToRadians
+        
+        let rotY = float4x4(
+            [cos(rad),  0,  -sin(rad),   0],
+            [       0,  1,          0,   0],
+            [sin(rad),  0,   cos(rad),   0],
+            [       0,  0,          0,   1]
+        )
+        
+        var uniforms = Uniforms()
+        
+        uniforms.modelMatrix = scale * rotY
+        
+        uniforms.viewMatrix = float4x4(
+            [1,   0,  0,   0],
+            [0,   1,  0,   0],
+            [0,   0,  1,   0],
+            [0,   0,  110, 1]
+        )
+        
+        uniforms.projectionMatrix = perspectiveProjection(fovy: Float(45).degreesToRadians, near: 0.001, far: 1000.0, aspect: aspect)
+        
+        
+        renderCommandEncoder.setVertexBytes(&uniforms,
+                                            length: MemoryLayout<Uniforms>.stride, index: 1)
+        
         
         renderCommandEncoder.drawPrimitives(type: .triangle,
                                             vertexStart: 0,
@@ -111,5 +141,19 @@ extension Renderer: MTKViewDelegate {
         // tells the system to replace the existing contents of the view with the newly-drawn contents when the GPU is done executing the previous commands
         commandBuffer.present(drawable)
         commandBuffer.commit()
+    }
+    
+    
+    func perspectiveProjection(fovy fov: Float, near: Float, far: Float, aspect: Float) -> matrix_float4x4 {
+        let y = 1 / tan(fov * 0.5)
+        let x = y / aspect
+        let z = far / (far - near)
+        let X = float4( x,  0,  0,  0)
+        let Y = float4( 0,  y,  0,  0)
+        let Z = float4( 0,  0,  z, 1)
+        let W = float4( 0,  0,  z * -near,  0)
+        var ret = matrix_identity_float4x4
+        ret.columns = (X, Y, Z, W)
+        return ret
     }
 }
--- METAL_MY/2 Triangle + Transform/Pipeline/Shaders.metal
+++ METAL_MY/2 Triangle + Transform/Pipeline/Shaders.metal
@@ -1,7 +1,7 @@
 
-
 #include <metal_stdlib>
 using namespace metal;
+#import "Common.h"
 
 struct VertexIn {
     float3 position [[attribute(0)]];
@@ -14,13 +14,14 @@ struct VertexOut {
 };
 
 vertex VertexOut vertex_main(VertexIn in [[stage_in]],
-                             constant float4x4 &matrix [[buffer(1)]])
+                             constant Uniforms &uniforms [[buffer(1)]])
 {
     VertexOut out;
-    out.position = matrix * float4(in.position, 1.0);
+    out.position = uniforms.projectionMatrix * uniforms.viewMatrix * uniforms.modelMatrix * float4(in.position, 1.0);
     out.color = float4(in.color, 1.0);
     return out;
 }
+
 
 fragment float4 fragment_main(VertexOut in [[stage_in]]) {
     return in.color;
